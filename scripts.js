b65eae2af4d03e1f00fef0851e17c4a5a5835948
Hanriiel <hannariikka.happonen@gmail.com>
2024-11-25 18:58:41 +0200

    muutokset

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-function setTime() {
+function updateTime() {
   const now = new Date();
-  
+
   const time = now.toLocaleTimeString('fi-FI');
 
   const dateOptions = {
@@ -11,7 +11,10 @@ function setTime() {
   };
   const date = now.toLocaleDateString('fi-FI', dateOptions);
 
+
   const datetime = `<p>Tänään on ${date}</p><p>Kello on ${time}</p>`;
 
   document.getElementById("datetime").innerHTML = datetime;
 }
+
+setInterval(updateTime, 1000);
